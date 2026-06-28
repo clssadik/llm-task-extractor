83d3352ebd1cc4a8c92614006a6e554d1ced0cd7
clssadik <cillsadik@gmail.com>
2026-06-28 17:39:54 +0300

    optional informations added

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr,ValidationError
+from datetime import datetime
 
 class User(BaseModel):
-    name: str
-    email: str
-    account_id: int
+    username: str
+    email: EmailStr
+    age: int
+    bio: str = ""
+    is_active: bool = True
 
-user = User(name="sadik",email="cillsadik@gmail.com",account_id=1)
-print(user)
+user1 = User(username="clssadik",email="cillsadik@gmail.com",age=23)
+print(user1)
